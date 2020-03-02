a9198aa200a10a36e6b110f86c3c6f9b39ee0b46
Ashish <bansodeashish24@gmail.com>
2020-03-02 10:13:58 +0530

    Structural directives introduction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  template: '<div>Hi</div>',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
+  // styles: ['div { background-color: blue;}']
 })
 export class AppComponent {
   title = 'fist-angular-app';
+  welcomeMessage = 'Here is our ISSC class:';
+  score = 100;
+
+  student = {
+    name: 'Gaurav',
+    age: 20
+  };
+
+  students = ['Rohan', 'Raju', 'Adu', 'Rahul', 'Nikita', 'Preksha', 'Vrushali'];
+
 }
