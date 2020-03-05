f317d246356a3b4e6a7759a6371ccaef4cf98030
Ashish <bansodeashish24@gmail.com>
2020-03-05 10:27:27 +0530

    Attribute directives and property-event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,4 +19,27 @@ export class AppComponent {
 
   students = ['Rohan', 'Raju', 'Adu', 'Rahul', 'Nikita', 'Preksha', 'Vrushali'];
 
+  teachers = [
+    {
+      name: 'Dr. Bedekar',
+      subject: 'Numerical Methods'
+    },
+    {
+      name: 'Tandon Sir',
+      subject: 'PPL'
+    }
+  ];
+
+  day = 'Sunday';
+
+  facebookImageLink = './assets/facebook-logo.png';
+
+  showWhiteBoldText = true;
+
+  sayHello() {
+    console.log('HELLO!!!!!');
+    this.showWhiteBoldText = !this.showWhiteBoldText;
+  }
+
+
 }
